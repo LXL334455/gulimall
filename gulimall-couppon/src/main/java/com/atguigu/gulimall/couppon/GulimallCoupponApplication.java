c2f00e5117d8d0d5c710e578051abc3f3bd388f5
mansingular <mansingular271@gmail.com>
2023-02-11 19:43:14 +0800

    项目初始结构创建

---
+++ gulimall-couppon/src/main/java/com/atguigu/gulimall/couppon/GulimallCoupponApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.couppon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCoupponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCoupponApplication.class, args);
+    }
+
+}
---
+++ gulimall-couppon/src/test/java/com/atguigu/gulimall/couppon/GulimallCoupponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.couppon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCoupponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
